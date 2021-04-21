cd3964e0cc09f7f37140b6725fe25e20538ed4bd
Luis J. Montes <luisjavm3@gmail.com>
2021-04-21 11:17:36 -0500

    xs header layout done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header">
-      <div className="title-wrapper">
-        <h1 className="title">Buscador</h1>
-      </div>
-      <div className="search-engine">
-        <div className="input-wrapper">
-          <input type="text" />
+      <div className="main-frame">
+        <div className="title-wrapper">
+          <h1 className="title">Buscador</h1>
         </div>
-        <div className="search-button-wrapper">
-          <button>Buscar</button>
+        <div className="search-engine">
+          <div className="input-wrapper">
+            <input type="text" />
+          </div>
+          <div className="search-button-wrapper">
+            <button>Buscar</button>
+          </div>
         </div>
       </div>
     </header>
