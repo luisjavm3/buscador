1f6c04018890e15e0a8b5ef5b2817d4dd39bbe9e
Luis J. Montes <luisjavm3@gmail.com>
2021-05-04 09:21:28 -0500

    searchTerm reducer completed

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,2 +1,5 @@
 export const SEARCH_RESULT = 'search';
 export const RESET_RESULT = 'resetResult';
+
+export const UPDATE_SEARCH_TERM = 'updateSearchTerm';
+export const RESET_SEARCH_TERM = 'resetSearchTerm';
--- src/redux/reducers/searchTerm.js
+++ src/redux/reducers/searchTerm.js
@@ -1,1 +1,14 @@
-export default function searchTerm(state = '', { type, payload }) {}
+import { RESET_SEARCH_TERM, UPDATE_SEARCH_TERM } from '../actionTypes';
+
+export default function searchTerm(state = '', { type, payload }) {
+  switch (type) {
+    case UPDATE_SEARCH_TERM:
+      return (state = payload.searchTerm);
+
+    case RESET_SEARCH_TERM:
+      return (state = '');
+
+    default:
+      break;
+  }
+}
